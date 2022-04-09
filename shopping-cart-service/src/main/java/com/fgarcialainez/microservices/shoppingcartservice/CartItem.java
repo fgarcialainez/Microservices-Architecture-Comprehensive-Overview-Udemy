51fdca572e15bae1feec998fe3883e2d7b5dd1b8
Felix Garcia Lainez <fgarcialainez@gmail.com>
2022-04-09 13:04:45 +0200

    Included a new shopping cart microservice which will interact with the existing product catalog microservice

---
+++ shopping-cart-service/src/main/java/com/fgarcialainez/microservices/shoppingcartservice/CartItem.java
@@ -0,0 +1,39 @@
+package com.fgarcialainez.microservices.shoppingcartservice;
+
+public class CartItem {
+    String productId;
+    int quantity;
+    double totalItemPrice;
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getTotalItemPrice() {
+        return totalItemPrice;
+    }
+
+    public void setTotalItemPrice(double totalItemPrice) {
+        this.totalItemPrice = totalItemPrice;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    @Override
+    public String toString() {
+        return "ShoppingCartItem [productId=" + productId + ", quantity=" + quantity + ", totalItemPrice="
+                + totalItemPrice + "]";
+    }
+
+
+}
---
+++ shopping-cart-service/src/main/java/com/fgarcialainez/microservices/shoppingcartservice/ShoppingCartService.java
@@ -0,0 +1,38 @@
+package com.fgarcialainez.microservices.shoppingcartservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+public class ShoppingCartService {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Autowired
+    ShoppingCartDao shoppingCartDao;
+
+    @PostMapping("/cart/{cartId}/item")
+    public Cart addItem(@PathVariable String cartId, @RequestBody CartItem item) {
+        if (cartId != null && item != null && item.getProductId() != null) {
+            // get product details
+            Product itemProduct = restTemplate.getForObject("http://localhost:8080/product/" + item.getProductId(),
+                    Product.class);
+
+            if (itemProduct != null && itemProduct.id != null) {
+                // adding total item price in the shopping cart item
+                item.setTotalItemPrice(itemProduct.getUnitPrice() * item.quantity);
+                return shoppingCartDao.addItem(cartId, item);
+            }
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "item product not found");
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "cart or item missing");
+    }
+
+}
